f936cb6c016eb5c80b8036014d5e02362bfa8864
angelikavolaric <volaric.angelika@gmail.com>
2025-01-11 23:37:14 +0100

    popravljanje storitevDeljenje, da dela

---
+++ GGG_project/frontend/goalgettergroup/src/app/app.routes.module.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {KarticeSeznamiComponent} from './kartice/karticeSeznami.component';
+
+export const routes: Routes = [
+
+  { path: '', component: KarticeSeznamiComponent },
+];
