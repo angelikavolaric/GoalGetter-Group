99fb08ba415bb9d7f2505371efbaf7fb04546202
angelikavolaric <volaric.angelika@gmail.com>
2025-01-13 02:56:08 +0100

    delovanje storitevKartice in storitevUporabnik v dockerju

--- GGG_project/storitevDeljenje/api-deljenje/src/main/java/si/fri/ggg/deljenje/api/v1/viri/DeljenjeViri.java
+++ GGG_project/storitevDeljenje/api-deljenje/src/main/java/si/fri/ggg/deljenje/api/v1/viri/DeljenjeViri.java
@@ -91,7 +91,7 @@ public class DeljenjeViri {
     @GET
     @Path("/{ciljId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUre(@PathParam("ciljId") int ciljId) {
+    public Response getCilj(@PathParam("ciljId") int ciljId) {
         try {
             CiljZrno clijZrno = new CiljZrno();
             Cilj cilj = clijZrno.pridobiCilj(ciljId);
@@ -112,7 +112,7 @@ public class DeljenjeViri {
     @PUT
     @Path("/{ciljId}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response putUre(@PathParam("ciljId") int ciljId, Cilj novCilj) {
+    public Response putCilj(@PathParam("ciljId") int ciljId, Cilj novCilj) {
         try {
             CiljZrno ciljZrno = new CiljZrno();
             Cilj posodobljenCilj = ciljZrno.urediCilj(ciljId, novCilj);
@@ -133,18 +133,19 @@ public class DeljenjeViri {
     @DELETE
     @Path("/{ciljId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteUre(@PathParam("ciljId") int ciljId) {
+    public Response deleteCilj(@PathParam("ciljId") int ciljId) {
 
 
         try {
             CiljZrno ciljZrno = new CiljZrno();
             boolean result = ciljZrno.odstraniCilj(ciljId);
-            if (!result) {
+            if (result) {
+                return Response.status(Response.Status.NO_CONTENT).build();
+            } else {
                 return Response.status(Response.Status.NOT_FOUND)
-                        .entity("Study goal with ID " + ciljId + " not found.")
+                        .entity("User with ID " + ciljId + " not found")
                         .build();
             }
-            return Response.status(Response.Status.NO_CONTENT).build();
         } catch (Exception e) {
             log.severe("Error deleting study goal with ID " + ciljId + ": " + e.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
@@ -153,6 +154,76 @@ public class DeljenjeViri {
         }
     }
 
+    private static final String BASE_URL = "http://localhost:8083/v1/ure/uporabnik";
+    @GET
+    @Path("/napredek/{uporabnikId}")
+    public Response getUre(@PathParam("uporabnikId") int uporabnikId){
+        Cilj aaa = new Cilj();
+        try {
+            CiljZrno ciljZrno = new CiljZrno();
+            Cilj result = ciljZrno.pridobiCiljeUp(uporabnikId);
+            if (result == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("Study goal with userID " + uporabnikId + " not found.")
+                        .build();
+            }
+            aaa = result;
+        } catch (Exception e) {
+            log.severe("Error finding study goal with userID " + uporabnikId + ": " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An error occurred while searching the study goal.")
+                    .build();
+        }
 
+        Client client = ClientBuilder.newClient();
+
+        WebTarget target = client.target(BASE_URL).path(""+uporabnikId);
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        if (response.getStatus() == 200) {
+
+
+            List<Object[]> ure = response.readEntity(List.class);
+            //List<Object[]> sums =(uporabnikId);
+
+            if (!ure.isEmpty()) {
+                Object[] sumRow = ure.get(0);  // Get the first (and only) row
+                Double sumUre = (Double) sumRow[0];  // Sum of 'vnosiUr'
+                Double sumMin = (Double) sumRow[1];  // Sum of 'vnosiMin'
+
+                Double doddatne = sumMin/60;
+                Double ost = sumMin%60;
+                sumMin = ost;
+                sumUre += doddatne;
+
+                Double ur = (double) aaa.getCiljUr();
+                Double m = (double) aaa.getCiljMin();
+                boolean isk = false;
+                if(ur < sumUre){
+                    isk = true;
+                }else if(ur == sumUre){
+                    if(m >= sumMin){
+                        isk = true;
+                    }
+                }
+
+                /*System.out.println("Sum of Ure: " + sumUre);
+                System.out.println("Sum of Min: " + sumMin);*/
+                String rez = "{ \"napredek učenja ure\": " + sumUre + ",  \"napredek učenja minute\": " + sumMin +
+                        ",  \"trenuten cilj ure\": " + aaa.getCiljUr() + ",  \"trenuten cilj minute\": " + aaa.getCiljMin()
+                        + ", \"cilj dosezen?\": " + isk + "}";
+                response.close();
+                return Response.ok(rez).build();
+            }
+            response.close();
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Study hours from user with userID " + uporabnikId + " not found.")
+                    .build();
+
+            //return ure;
+        } else {
+            // Handle error (you might want to throw an exception or handle the error differently)
+            throw new RuntimeException("Failed to fetch hours: " + response.getStatus());
+        }
+    }
 
 }
