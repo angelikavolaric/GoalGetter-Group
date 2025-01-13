87e1d3988ba73d3d5a5845ffea559261f035d6eb
angelikavolaric <volaric.angelika@gmail.com>
2025-01-13 13:39:43 +0100

    .

--- .idea/storitevObvestila/entitete-Obvestila/target/classes/sql-scripts/init-db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS obvestila
-s (id SERIAL PRIMARY KEY, outerAPIid VARCHAR(255) NOT NULL, opis TEXT NOT NULL, zacetek TEXT NOT NULL, konec TEXT NOT NULL);
-
-
-INSERT INTO obvestila
-s (outerAPIid, opis, zacetek, konec) VALUES ('6783be8c5be34b0f9660d627', 'Test Obvestila
-', '2025-01-12T14:00:00Z', '2025-01-12T15:00:00Z' );
